b3b12bae4048b24cb821e98998741e90351e39ad
Adrian Adshead <adrian.adshead@singlewire.com>
2022-04-22 10:42:49 -0500

    Add extra tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,14 @@
+def test_success1():
+    print('Hello World1!')
+    assert 1 == 1
+    print('Goodbye World1!')
+    
 def test_main():
     print('Hello World!')
     assert 1 == 2
     print('Goodbye World!')
+
+def test_success2():
+    print('Hello World2!')
+    assert 2 == 2
+    print('Goodbye World2!')
